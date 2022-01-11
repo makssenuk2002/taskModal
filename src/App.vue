a07fd4b3a8c8a19ef2f001d4df9a62f3f22aab7c
Maks <makssenuk2002@gmail.com>
2022-01-11 18:39:51 +0300

    modal

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button class="btn btn--primary mx-auto" @click="$refs.modalName.openModal()">Open modal</button>
+    <modal ref="modalName"
+      :TITLE="TITLE"
+      :BODY="BODY"
+      :SUCCESS_BTN_TEXT="SUCCESS_BTN_TEXT"
+      :CANCEL_BTN_TEXT="CANCEL_BTN_TEXT"
+      :BACKDROP_CLOSE="true"
+    >
+    </modal>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      TITLE:'TITLE',
+      BODY:'BODY',
+      SUCCESS_BTN_TEXT:'Save',
+      CANCEL_BTN_TEXT:'Cancel',
+    };
+  },
+};
+</script>
+
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <transition name="fade">
+    <div class="modal" v-if="show">
+      <div class="modal__backdrop" @click="closeModalBackdrop()"/>
+
+      <div class="modal__dialog">
+        <div class="modal__header">
+          <h1>{{TITLE}}</h1>
+        </div>
+
+        <div class="modal__body">
+          <h4>{{BODY}}</h4>
+        </div>
+
+        <div class="modal__footer">
+          <div class="d-flex align-items-center justify-content-between">
+            <button class="btn btn--secondary" @click="closeModal()">{{CANCEL_BTN_TEXT}}</button>
+            <button class="btn btn--primary" @click="closeModal()">{{SUCCESS_BTN_TEXT}}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props:['TITLE','BODY','SUCCESS_BTN_TEXT','CANCEL_BTN_TEXT','BACKDROP_CLOSE'],
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    closeModal() {
+      this.show = false;
+    },
+    closeModalBackdrop() {
+      if (this.BACKDROP_CLOSE){
+        this.show = false;
+      }
+    },
+    openModal() {
+      this.show = true;
+    }
+  }
+};
+</script>
